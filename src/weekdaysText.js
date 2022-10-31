118f73d09c29307d1bfd5aa3c470bc437e58c003
Ebrahim Sobeh <58515285+ebrahimsobeh@users.noreply.github.com>
2022-10-31 11:41:33 +0200

    Update weekdaysText.js

--- src/weekdaysText.js
+++ src/weekdaysText.js
@@ -1,14 +1,10 @@
 function weekdaysText(weekdays) {
     let day = '';
     return function getText(target) {
-        if (target > weekdays.length || target < 0) {
+        if (target > weekdays.length || target < 0)
             return 'Invalid day number';
-        } else {
-            for (let i = 0; i < weekdays.length; i++) {
-                day = weekdays[target];
-            }
-            
-            return day;
-        }
+        day = weekdays[target];
+        return day;
+        
     }
 }
